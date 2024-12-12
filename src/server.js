d8bbad711cd8a8dd3d604a32f0ac12a5342ff26e
Davi Bittencourt <dbittencourt2002@gmail.com>
2024-12-12 16:11:44 -0300

    fix: corrige envio de resposta em json da api

--- src/server.js
+++ src/server.js
@@ -11,21 +11,21 @@ app.use(express.json());
 const historico = [];
 
 app.get("/tecnologia", (req, res) => {
-    res.send(obterTecnologiaAleatoria());
+  res.json(obterTecnologiaAleatoria());
 });
 
 app.post("/batalha", (req, res) => {
-    // TODO: Implementar a lógica da batalha
+  // TODO: Implementar a lógica da batalha
 });
 
 app.post("/batalha/historico", (req, res) => {
-    // TODO: Implementar a lógica de adição de novo resultado no histórico de batalhas
+  // TODO: Implementar a lógica de adição de novo resultado no histórico de batalhas
 });
 
 app.get("/batalha/historico", (req, res) => {
-    // TODO: Implementar a lógica de obtenção de histórico de batalhas
+  // TODO: Implementar a lógica de obtenção de histórico de batalhas
 });
 
 app.listen(port, () => {
-    console.log(`Servidor iniciado em: http://localhost:${port}`);
+  console.log(`Servidor iniciado em: http://localhost:${port}`);
 });
