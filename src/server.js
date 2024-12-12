3725c5835062f2437687fa1745b74508c62e37de
Davi Bittencourt <dbittencourt2002@gmail.com>
2024-12-11 22:37:57 -0300

    feat: adiciona rotas a serem utilizadas na solução

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import { obterTecnologiaAleatoria } from "./data.js";
+
 import express from "express";
 
 const app = express();
@@ -5,6 +7,24 @@ const port = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
+
+const historico = [];
+
+app.get("/tecnologia", (req, res) => {
+    res.send(obterTecnologiaAleatoria());
+});
+
+app.post("/batalha", (req, res) => {
+    // TODO: Implementar a lógica da batalha
+});
+
+app.post("/batalha/historico", (req, res) => {
+    // TODO: Implementar a lógica de adição de novo resultado no histórico de batalhas
+});
+
+app.get("/batalha/historico", (req, res) => {
+    // TODO: Implementar a lógica de obtenção de histórico de batalhas
+});
 
 app.listen(port, () => {
     console.log(`Servidor iniciado em: http://localhost:${port}`);
